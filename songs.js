c987fd848674a915ebddea351d86e8742a351b73
Debra Gordon <debra.m.gordon@gmail.com>
2016-08-11 18:35:44 -0500

    adding initial files

---
+++ songs.js
@@ -0,0 +1,12 @@
+var songs = [];
+
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+// Each student must add one song to the beginning and the end of the array.
+// Loop over the array and remove any words or characters that obviously don't belong.
+// Students must find and replace the > character in each item with a - character.
+// Must add each string to the DOM in index.html in the main content area.
